44d974f5878b4c61f60a8a38de5894aa913d9f43
Moja <moja3638442@gmail.com>
2021-04-11 14:46:53 +0000

    :bug: Fix: staticフォルダの場所修正
    srcの外に出す

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,12 @@ export default() => (
     <title>ESSENTIALS</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" crossOrigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
-    <link rel="icon" href="images/icon.png" type="image/png" />
+    <link rel="icon" href="/images/icon.png" type="image/png" />
     <header className="header">
       <div className="container">
         <div className="site">
           <a href="base-index.html">
-            <img src="images/logo.svg" alt="ESSENTIALS" />
+            <img src="/images/logo.svg" alt="ESSENTIALS" />
           </a>
         </div>
         <nav className="nav">
@@ -25,14 +25,14 @@ export default() => (
     </header>
     <section className="hero">
       <figure>
-        <img src="images/hero.jpg" alt="" />
+        <img src="/images/hero.jpg" alt="" />
       </figure>
       <div className="catch">
         <h1>There is no love sincerer than<br /> the love of food.</h1>
         <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
       </div>
       <div className="wave">
-        <img src="images/wave.svg" alt="" />
+        <img src="/images/wave.svg" alt="" />
       </div>
     </section>
     <section className="food">
@@ -41,7 +41,7 @@ export default() => (
         <div className="details">
           <div className="detail">
             <figure>
-              <img src="images/fruit.jpg" alt="" />
+              <img src="/images/fruit.jpg" alt="" />
             </figure>
             <h3>フルーツ</h3>
             <p>FRUIT</p>
@@ -49,7 +49,7 @@ export default() => (
           </div>
           <div className="detail">
             <figure>
-              <img src="images/grain.jpg" alt="" />
+              <img src="/images/grain.jpg" alt="" />
             </figure>
             <h3>穀物</h3>
             <p>GRAIN</p>
@@ -57,7 +57,7 @@ export default() => (
           </div>
           <div className="detail">
             <figure>
-              <img src="images/beverage.jpg" alt="" />
+              <img src="/images/beverage.jpg" alt="" />
             </figure>
             <h3>飲み物</h3>
             <p>BEVERAGE</p>
@@ -69,14 +69,14 @@ export default() => (
     <section className="photo">
       <h2 className="sr-only">Photo</h2>
       <figure>
-        <img src="images/berry.jpg" alt="赤く熟したベリー" />
+        <img src="/images/berry.jpg" alt="赤く熟したベリー" />
       </figure>
     </section>
     <footer className="footer">
       <div className="container">
         <div className="site">
           <a href="base-index.html">
-            <img src="images/logo-w.svg" alt="ESSENTIALS" />
+            <img src="/images/logo-w.svg" alt="ESSENTIALS" />
             <p>おいしい食材と食事を探求するサイト</p>
           </a>
         </div>
