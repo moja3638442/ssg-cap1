da74d5fcad1c6417a0e6010b8b83fcc5b3d35416
Moja <moja3638442@gmail.com>
2021-04-12 11:08:07 +0000

    :heavy_plus_sign: Add: GraphQLで使用するimage系プラグインを追加
    yarnでインストールを実施した

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "mysite",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
